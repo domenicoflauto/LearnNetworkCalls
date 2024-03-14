2b0fed100badd85959b04f499a09fa3829d717db
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-03-14 12:15:17 +0000

    Initial Commit

---
+++ LearnNetworkCalls/LearnNetworkCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnNetworkCallsApp.swift
+//  LearnNetworkCalls
+//
+//  Created by Domenico Flauto on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearnNetworkCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
